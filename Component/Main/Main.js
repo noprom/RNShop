f70f8ff767bf8d6d8c3cfd2019dcdc226d6589be
noprom <tyee.noprom@qq.com>
2016-11-23 15:36:51 +0800

    init main framework

--- Component/Main/Main.js
+++ Component/Main/Main.js
@@ -3,38 +3,87 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Platform
 } from 'react-native';
 
+// 引入外部组件
+import TabNavigator from 'react-native-tab-navigator';
+var Home = require('../Home/Home');
+var Shop = require('../Shop/Shop');
+var Mine = require('../Mine/Mine');
+var More = require('../More/More');
+
 var Main = React.createClass({
+  // 初始化函数
+  getInitialState() {
+    return {
+      selectedTab: 'home'
+    }
+  },
+
   render() {
     return (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Main
-          </Text>
-        </View>
-      );
+        <TabNavigator>
+            {/*--首页--*/}
+            <TabNavigator.Item
+                title="首页"
+                renderIcon={() => <Image source={require('../image/icon/tabbar/homepage.png')} style={styles.iconStyle} />}
+                renderSelectedIcon={() => <Image source={require('../image/icon/tabbar/homepage_selected.png')} style={styles.selectedIconStyle} />}
+                badgeText="1"
+                selected={this.state.selectedTab === 'home'}
+                onPress={() => this.setState({ selectedTab: 'home' })}
+            >
+                <Home />
+            </TabNavigator.Item>
+            {/*--商家--*/}
+            <TabNavigator.Item
+                title="商家"
+                renderIcon={() => <Image source={require('../image/icon/tabbar/merchant_normal.png')} style={styles.iconStyle} />}
+                renderSelectedIcon={() => <Image source={require('../image/icon/tabbar/merchant_selected.png')} style={styles.selectedIconStyle} />}
+                badgeText="1"
+                selected={this.state.selectedTab === 'shop'}
+                onPress={() => this.setState({ selectedTab: 'shop' })}
+            >
+                <Shop />
+            </TabNavigator.Item>
+            {/*--我的--*/}
+            <TabNavigator.Item
+                title="我的"
+                renderIcon={() => <Image source={require('../image/icon/tabbar/mine.png')} style={styles.iconStyle} />}
+                renderSelectedIcon={() => <Image source={require('../image/icon/tabbar/mine_selected.png')} style={styles.selectedIconStyle} />}
+                badgeText="1"
+                selected={this.state.selectedTab === 'mine'}
+                onPress={() => this.setState({ selectedTab: 'mine' })}
+            >
+                <Mine />
+            </TabNavigator.Item>
+            {/*--更多--*/}
+            <TabNavigator.Item
+                title="更多"
+                renderIcon={() => <Image source={require('../image/icon/tabbar/misc.png')} style={styles.iconStyle} />}
+                renderSelectedIcon={() => <Image source={require('../image/icon/tabbar/misc_selected.png')} style={styles.selectedIconStyle} />}
+                badgeText="1"
+                onPress={() => this.setState({ selectedTab: 'more' })}
+                selected={this.state.selectedTab === 'more'}
+            >
+                <More />
+            </TabNavigator.Item>
+            </TabNavigator>
+    );
   }
 });
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  iconStyle:{
+        width: Platform.OS === 'ios' ? 30 : 25,
+        height:Platform.OS === 'ios' ? 30 : 25,
+    },
+    selectedIconStyle:{
+        width:Platform.OS === 'ios' ? 30 : 25,
+        height:Platform.OS === 'ios' ? 30 : 25,
+    },
 });
 
 // 输出
---
+++ Component/Shop/Shop.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Platform
+} from 'react-native';
+
+var Shop = React.createClass({
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Shop
+          </Text>
+        </View>
+      );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+// 输出
+module.exports = Shop;
