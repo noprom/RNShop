73fa50be1d00e76c7492e9b9db87813ffd3cb4bd
noprom <tyee.noprom@qq.com>
2016-11-23 16:41:45 +0800

    optimize icons

--- Component/Home/Home.js
+++ Component/Home/Home.js
@@ -4,13 +4,40 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput,
+  Image,
+  Platform
 } from 'react-native';
+
+var Dimensions = require('Dimensions');
+var screenW = Dimensions.get('window').width;
+var screenH = Dimensions.get('window').height;
 
 // 引入外部组件
 var HomeDetail = require('./HomeDetail');
 
 var Home = React.createClass({
+  // 首页的导航条
+  renderNavBar() {
+    return(
+        <View style={styles.navBarStyle}>
+          <TouchableOpacity onPress={()=>{this.pushToDetail()}} >
+            <Text style={styles.leftTitleStyle}>宁波</Text>
+          </TouchableOpacity>
+          <TextInput placeholder="输入商家,品类,商圈" style={styles.topInputStyle} />
+          <View style={styles.rightNavViewStyle}>
+            <TouchableOpacity onPress={()=>{alert('点击了')}} >
+              <Image source={{uri:'icon_homepage_message'}} style={styles.navRightImgStyle} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>{alert('点击了')}} >
+                <Image source={{uri:'icon_homepage_scan'}} style={styles.navRightImgStyle} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )
+  },
+
   // 跳转到详情页面
   pushToDetail() {
     this.props.navigator.push({
@@ -22,6 +49,8 @@ var Home = React.createClass({
   render() {
     return (
         <View style={styles.container}>
+          {/*首页的导航条*/}
+          {this.renderNavBar()}
           <TouchableOpacity onPress={() => {this.pushToDetail()}}>
             <Text style={styles.welcome}>
               Home
