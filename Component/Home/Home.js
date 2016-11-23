3b948d2c69c461db7bcc0a6b10c0e7745e45c302
noprom <tyee.noprom@qq.com>
2016-11-23 16:54:55 +0800

    add some styles

--- Component/Home/Home.js
+++ Component/Home/Home.js
@@ -62,6 +62,44 @@ var Home = React.createClass({
 });
 
 const styles = StyleSheet.create({
+  // 导航栏
+  navBarStyle:{
+      height:Platform.OS === 'ios' ? 60 : 44,
+      backgroundColor:'rgba(255,96,0,1)',
+      // 主轴方向
+      flexDirection:'row',
+      // 侧轴对齐方式 垂直居中
+      alignItems:'center',
+      // 主轴对齐方式
+      justifyContent:'space-around', // 平均分布
+  },
+  // 导航条左侧文字
+  leftTitleStyle:{
+      color:'white',
+  },
+  // 导航栏输入框
+  topInputStyle:{
+      width:screenW * 0.70,
+      height:Platform.OS === 'ios' ? 35 : 30,
+      backgroundColor:'white',
+      marginTop:Platform.OS === 'ios' ? 18 : 0,
+      // 圆角
+      borderRadius:18,
+      paddingLeft:10,
+  },
+  // 导航条右侧视图
+  rightNavViewStyle:{
+      flexDirection:'row',
+      height:64,
+      // 侧轴对齐方式
+      alignItems:'center',
+      // backgroundColor:'blue',
+  },
+  // 导航栏右侧图片
+  navRightImgStyle:{
+      width:Platform.OS === 'ios' ? 28 : 24,
+      height:Platform.OS === 'ios' ? 28 : 24,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -72,11 +110,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
